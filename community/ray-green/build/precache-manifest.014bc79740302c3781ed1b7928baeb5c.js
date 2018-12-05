9528eb2886bdb739d609f4ac6ebebd2525819e48
rgreen32 <ray.greenm@gmail.com>
2018-12-05 11:29:34 -0600

    Rey's app

---
+++ community/ray-green/build/precache-manifest.014bc79740302c3781ed1b7928baeb5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9e6889db37e3035b92",
+    "url": "/ray-green/build/static/js/runtime~main.8d9e6889.js"
+  },
+  {
+    "revision": "62bd5093e042fb403c0d",
+    "url": "/ray-green/build/static/js/main.62bd5093.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/ray-green/build/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "62bd5093e042fb403c0d",
+    "url": "/ray-green/build/static/css/main.354f5ebb.chunk.css"
+  },
+  {
+    "revision": "a8150ee9d94db9b416b29c627d252786",
+    "url": "/ray-green/build/index.html"
+  }
+];
