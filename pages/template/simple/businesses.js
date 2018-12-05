e7ea2e9446ed008feb98f0915f78a3dfb41c98e1
Bryce Sharp <bryce@gorocketfuel.com>
2018-12-05 01:52:02 -0600

    Created simple react app

--- pages/template/simple/businesses.js
+++
@@ -1,5 +0,0 @@
-$.getJSON('https://blacklatinoit.com/api/businesses', function(businesses){
-    $.each(businesses, function( key, business ){
-        $('#simple-businesses').append('<li>' + business['name'] + '</li>');
-    });
-});
