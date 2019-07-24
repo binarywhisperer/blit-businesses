38527130917c8549134903ede9dba8f20e5b6919
Joshtg1104 <joshtg1104@gmail.com>
2019-07-24 14:03:57 -0500

    API, it baffled me for a while

---
+++ community/joshuatg/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            businesses: [],
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                BLiT Businesses
+                            </h1>
+                            <h2 className="subtitle">
+                                by Joshua G.
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+                <hr/>
+                {this.state.businesses.map(business =>
+                        <div className="card" key={business.objectID}>
+                            <div className="card-content">
+                                <p className="title">
+                                    {business['name']}
+                                </p>
+                                <p className="subtitle">
+                                    {business['owner']}
+                                    <br/>
+                                    {business['email']}
+                                    <br/>
+                                    {business['phone']}
+                                </p>
+                            </div>
+                            <footer className="card-footer">
+                                <p className="card-footer-item">
+            <span>
+            <a href="{business.url}">View Website</a>
+            </span>
+                                </p>
+                            </footer>
+                        </div>
+                )}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch('https://blacklatinoit.com/api/businesses')
+            .then(response => response.json())
+            .then(data => {
+                let result = Object.keys(data).map((key) => {
+                    return data[key];
+                });
+                console.log(result);
+                this.setState({ businesses: result })
+            });
+    }
+}
+
+export default App;
